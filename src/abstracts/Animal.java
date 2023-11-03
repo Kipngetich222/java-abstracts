88e508bb128a7d763f97b53317226160caa31fc0
Kipngetich222 <123062517+Kipngetich222@users.noreply.github.com>
2023-11-03 08:25:36 +0300

    java abstracts

---
+++ src/abstracts/Animal.java
@@ -0,0 +1,18 @@
+package abstracts;
+
+public abstract class Animal {
+	public String name;
+	public abstract void makeSound();
+	public abstract void move();
+	public Animal(String animalName) {
+		name= animalName;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public String toString() {
+		return getName();
+	}
+
+}
---
+++ src/abstracts/Cat.java
@@ -0,0 +1,17 @@
+package abstracts;
+
+public class Cat extends Animal {
+
+	@Override
+	public void makeSound() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
